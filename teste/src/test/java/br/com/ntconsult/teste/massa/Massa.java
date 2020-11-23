72ec09aa00b30f17a367039e4e3194a2a73d09d2
Deivison Marins <deivison.mar@gmail.com>
2020-11-22 21:19:08 -0300

    Add files via upload

---
+++ teste/src/test/java/br/com/ntconsult/teste/massa/Massa.java
@@ -0,0 +1,10 @@
+package br.com.ntconsult.teste.massa;
+
+public class Massa {
+	
+	protected String api = "http://jsonplaceholder.typicode.com/";
+	
+	protected String body1 = "4";
+	protected String body2 = "45";
+	protected String body3 = "65";
+}
